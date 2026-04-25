b93923020a93226065e7d1976c48a6c59e1bf9ad
matteo silla <96153074+teosssss@users.noreply.github.com>
2026-04-25 18:57:30 +0200

    fix post-merge build issues

--- ggml/src/ggml-metal/ggml-metal-device.m
+++ ggml/src/ggml-metal/ggml-metal-device.m
@@ -1276,13 +1276,6 @@ bool ggml_metal_device_supports_op(ggml_metal_device_t dev, const struct ggml_te
                            case GGML_TYPE_I32:
                            case GGML_TYPE_TURBO2_0:
                            case GGML_TYPE_TURBO3_0:
-                           case GGML_TYPE_TURBO4_0:
-                                return true;
-                           case GGML_TYPE_Q5_1:
-                           case GGML_TYPE_IQ4_NL:
-                           case GGML_TYPE_I32:
-                           case GGML_TYPE_TURBO2_0:
-                           case GGML_TYPE_TURBO3_0:
                            case GGML_TYPE_TURBO3_EMPVAR_0:
                            case GGML_TYPE_TURBO3_PCA_0:
                            case GGML_TYPE_TURBO4_0:
